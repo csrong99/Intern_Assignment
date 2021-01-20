f26b2537e70cdc9315b7952d1ef0a942f04b1ac5
Chuah <chuahsr99@gmail.com>
2021-01-21 01:10:43 +0800

    Remove problem code

--- Assignment/Controllers/LoginController.cs
+++ Assignment/Controllers/LoginController.cs
@@ -16,26 +16,6 @@ namespace Assignment.Controllers
             return View();
         }
 
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public JsonResult ValidateUser(string username, string password, bool rememberme) {
-
-            if (ModelState.IsValid) {
-                string username = emp_login_view.Username;
-                string password = emp_login_view.Password;
-
-                Employee emp = db.Employees.Find(username);
-                if(emp != null) {
-                    if(emp.Password.Equals(password)) {
-                        return RedirectToRoute("EmployeeIndex");
-					}
-                    ModelState.AddModelError(string.Empty, "Invalid username or password");
-				}
-            }
-
-            return View();
-        }
 
     }
 }
